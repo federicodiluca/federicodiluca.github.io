2c93b98dab973093dbeda23f1c088fff0646ea85
Federico Di Luca <federico.diluca95@gmail.com>
2026-09-05 15:58:54 +0200

    vocabe

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,6 +2,6 @@ import type { APIRoute } from "astro";
 import { SITE_URL } from "../consts";
 
 export const GET: APIRoute = () => {
-  const body = `User-agent: *\nAllow: /\n\nSitemap: ${SITE_URL}/sitemap-index.xml\n`;
+  const body = `User-agent: *\nAllow: /\n\nSitemap: ${SITE_URL}/sitemap-index.xml\nSitemap: ${SITE_URL}/vocabe/sitemap.xml\n`;
   return new Response(body, { headers: { "Content-Type": "text/plain" } });
 };
