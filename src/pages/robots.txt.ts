755c523538a26652cfddafdea96551b5a2d75307
Federico Di Luca <federico.diluca95@gmail.com>
2026-09-25 18:28:04 +0200

    robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,6 +2,6 @@ import type { APIRoute } from "astro";
 import { SITE_URL } from "../consts";
 
 export const GET: APIRoute = () => {
-  const body = `User-agent: *\nAllow: /\n\nSitemap: ${SITE_URL}/sitemap-index.xml\nSitemap: ${SITE_URL}/vocabe/sitemap.xml\nSitemap: ${SITE_URL}/school-feed-monitor/sitemap.xml\n`;
+  const body = `User-agent: *\nAllow: /\n\nSitemap: ${SITE_URL}/sitemap-index.xml\nSitemap: ${SITE_URL}/vocabe/sitemap.xml\nSitemap: ${SITE_URL}/school-feed-monitor/sitemap.xml\nSitemap: ${SITE_URL}/la-scimmia-vince/sitemap-index.xml\n`;
   return new Response(body, { headers: { "Content-Type": "text/plain" } });
 };
